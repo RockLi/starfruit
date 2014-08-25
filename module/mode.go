0fcc50d12d06ef742a514ae8aab32b42892680ab
Rock Lee <insfocus@gmail.com>
2014-08-25 21:39:07 +0800

    tweak the license

--- module/mode.go
+++ module/mode.go
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 The flatpeach Authors. All rights reserved.
+ * Copyright 2014 The starfruit Authors. All rights reserved.
  * Use of this source code is governed by a BSD-style
  * license that can be found in the LICENSE file.
  */
